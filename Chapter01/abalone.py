cbbea015d6c3e72a071d6ef943f7922854d91f7a
absf123 <53507170+absf123@users.noreply.github.com>
2021-02-05 02:32:38 +0900

    chapter01 끝

--- Chapter01/abalone.py
+++ Chapter01/abalone.py
@@ -2,35 +2,24 @@ import numpy as np
 import csv   # pandas는?
 import time
 
-np.random.seed(1234)
-def randomize(): np.random.seed(time.time())    # 난수 함수 초기화
-
-# hyperparameter 정의
-RND_MEAN = 0            # 정규분포 난수값의 평균
-RND_STD = 0.0030        # 표준편차
-LERNING_RATE = 0.001
-
 
 # main function
 def abalone_exec(epochs=10, batch_size=10, report=1):
     load_abalone_dataset()      # dataset load
     init_model()                # model parameter 초기화
-    train_and_test(epoch, batch_size, report)              # 딥러닝 model 생성 및 전체학습과정 처리
-
-
-
+    train_and_test(epochs, batch_size, report)              # 딥러닝 model 생성 및 전체학습과정 처리
 
 
 # dataset load
 def load_abalone_dataset():
-    with open("./Data/abalone.csv") as csvfile:
+    with open("../Data/abalone.csv") as csvfile:
         csvreader = csv.reader(csvfile)
         next(csvreader, None)       # 파일의 첫 행을 읽지 않고 건너뜀
         rows = []
         for row in csvreader:       # rows list에 feature 정보 수집
             rows.append(row)
 
-    global data, input_cut, output_cnt    # 전역변수.... -> 고치는게 좋지 않을까?, 계속 사용하기위함이라긴 함
+    global data, input_cnt, output_cnt    # 전역변수.... -> 고치는게 좋지 않을까?, 계속 사용하기위함이라긴 함
     input_cnt, output_cnt = 10, 1         # 입출력 벡터 크기 10, 1로 선언
     data = np.zeros([len(rows), input_cnt+output_cnt])   # data 행렬 : 입출력 벡터 정보를 저장할 행렬 (input feature에 output_cnt 만큼 column 추가)
 
@@ -39,7 +28,7 @@ def load_abalone_dataset():
         if row[0] == 'I' : data[n, 0] = 1
         if row[1] == 'M' : data[n, 1] = 1
         if row[2] == 'F' : data[n, 2] = 1
-        data[n, 3] = row[1:]
+        data[n, 3:] = row[1:]
 
 # parameter 초기화
 def init_model():
@@ -58,7 +47,7 @@ def train_and_test(epochs, batch_size, report):
         # epoch당 step수 (train data 수 / mini_batch 수)
         for n in range(step_size):
             train_x, train_y = get_train_data(batch_size, n)
-            loss, acc = run_train(train_x, trian_y)
+            loss, acc = run_train(train_x, train_y)
             losses.append(loss)
             accs.append(acc)
 
@@ -80,8 +69,8 @@ def arrange_data(batch_size):
     return step_size
 
 def get_test_data():
-    global data, shuffle_map, test_begin_inx, output_cnt
-    test_data = data[shuffle_map[test_begin_inx:]]     # test data 생성
+    global data, shuffle_map, test_begin_idx, output_cnt
+    test_data = data[shuffle_map[test_begin_idx:]]     # test data 생성
     return test_data[:, :-output_cnt], test_data[:, -output_cnt:]   # test_x(입력 벡터), test_y(정답 벡터)
 
 def get_train_data(batch_size, nth):
@@ -92,14 +81,14 @@ def get_train_data(batch_size, nth):
     return train_data[:, :-output_cnt], train_data[:, -output_cnt:]    # train_x, train_y
 
 # per step process
-def run_trian(x, y):
+def run_train(x, y):
     output, aux_nn = forward_neuralnet(x)            # 순전파, 신경망 출력 결과
-    loss, aux_nn = forward_postproc(output, y)       # 순전파, loss 계산(predict과 ground truth 차이 오차)
+    loss, aux_pp = forward_postproc(output, y)       # 순전파, loss 계산(predict과 ground truth 차이 오차)
     accuracy = eval_accuracy(output, y)              # 정확도 계산
 
     G_loss = 1.0   # Loss에 대한 loss의 편미분 값 1 -> 역전파의 시작점
     G_output = backprop_postproc(G_loss, aux_pp)     # 역전파
-    backprop_neuralnet(G_loss, aux_nn)               # 역전파
+    backprop_neuralnet(G_output, aux_nn)               # 역전파
 
     return loss, accuracy
 
@@ -110,21 +99,83 @@ def run_test(x, y):
     return accuracy
 
 
+# 순전파 및 역전파 함수 정의
+def forward_neuralnet(x):
+    global weight, bias
+    output = np.matmul(x, weight) + bias
+    return output, x
 
+def backprop_neuralnet(G_output, x):
+    global weight, bias
+    g_output_w = x.transpose()
+    G_w = np.matmul(g_output_w, G_output)
+    G_b = np.sum(G_output, axis=0)
 
+    weight -= LERNING_RATE * G_w
+    bias -= LERNING_RATE * G_b
 
+# 후처리 과정에 대한 순전파 및 역전파 함수 정의
+def forward_postproc(output, y):
+    diff = output - y
+    loss = np.mean(np.square(diff))
+    return loss, diff
 
+# 순전파의 역방향
+def backprop_postproc(G_loss, diff):
+    shape = diff.shape
 
+    g_loss_square= np.ones(shape) / np.prod(shape)   # np.prod -> 배열 원소간 곱(axis 기준)
+    g_square_diff = 2 * diff   # diff^2 -> 2diff
+    g_diff_output = 1   # diff = output - y 의 ddiff / doutput = 1
 
+    G_square = g_loss_square * G_loss   # G_loss = 1.0
+    G_diff = g_square_diff * G_square
+    G_output = g_diff_output * G_diff
 
+    return G_output
+
+# 정확도 계산
+def eval_accuracy(output, y):
+    mdiff = np.mean(np.abs((output - y)/y))  # 오류율 평균
+    return 1 - mdiff
 
 
 
 
 if __name__ == "__main__":
     print("TRAIN START!")
-    epochs = 10              # epoch 수
-    batch_size = 10          # 미니배치 수
-    report = 1               # 결과 출력 주기
+    np.random.seed(1234)
+
+
+    def randomize(): np.random.seed(time.time())  # 난수 함수 초기화
+
+
+    # hyperparameter 정의
+    RND_MEAN = 0  # 정규분포 난수값의 평균
+    RND_STD = 0.0030  # 표준편차
+    LERNING_RATE = 0.1
+
+
+    epochs = 100              # epoch 수
+    batch_size = 100          # 미니배치 수
+    report = 20            # 결과 출력 주기
 
     abalone_exec(epochs=epochs, batch_size=batch_size, report=report)
+    print()
+    print(weight)
+    print(bias)
+
+    """
+    [[ 0.28084865]  weight
+     [-0.00357293]
+     [ 0.00429812]
+     [ 2.33109724]
+     [ 1.85242646]
+     [ 0.68413302]
+     [ 2.80611213]
+     [ 0.70162155]
+     [ 0.56256811]
+     [ 1.14419111]]
+     
+    [4.72213148]  bias
+    """
--- readme.txt
+++ readme.txt
@@ -5,4 +5,5 @@ https://github.com/konantechnology/academy.alzza
 열심히 하자
 2021.2.3 시작
 
-# 2021.2.3   chapter01 abalone 전복 나이 추정, 단순 perceptron으로 선형회귀 문제 해결  ~ p77
+- 2021.2.3   chapter01 abalone 전복 나이 추정, 단순 perceptron으로 선형회귀 문제 해결  ~ p77
+- 2021.2.4   chapter01 끝, 해볼만 한 것 -> multi-layer로 만들어보기
