8d8cc1ca41437bfb7028fb4415b3ba5649c8ea06
absf123 <53507170+absf123@users.noreply.github.com>
2021-02-10 02:10:26 +0900

    chapter02 끝 2.9

--- Chapter02/pulsar.py
+++ Chapter02/pulsar.py
@@ -21,21 +21,24 @@ def train_and_test(epochs, batch_size, report):
     test_x, test_y = get_test_data()
 
     for epoch in range(epochs):
-        losses, accs = [], []
+        losses = []
         # epoch당 step수 (train data 수 / mini_batch 수)
         for n in range(step_size):
             train_x, train_y = get_train_data(batch_size, n)
-            loss, acc = run_train(train_x, train_y)
+            loss, _ = run_train(train_x, train_y)
             losses.append(loss)
-            accs.append(acc)
+            # accs.append(acc)
 
         if report > 0 and (epoch+1) % report == 0:
             acc = run_test(test_x, test_y)
-            print("EPOCH {}: LOSS={:5.3f}, ACC={:5.3f}/{:5.3f}".\
-                  format(epoch+1, np.mean(losses), np.mean(accs), acc))    # np.mean(accs) : 매 epoch시 test acc의 평균값, acc : 직전 epoch의 test acc값
+            acc_str = ','.join(['%5.3f']*4) % tuple(acc)        # eval_accuracy의 출력 결과 수와 포멧에 맞게 문자열로 수정
+            print("EPOCH {}: LOSS={:5.3f}, RESULT={}".\
+                  format(epoch+1, np.mean(losses), acc_str))    # np.mean(accs) : 매 epoch시 test acc의 평균값, acc : 직전 epoch의 test acc값
 
-    final_acc = run_test(test_x, test_y)
-    print('\nFinal Test : final accuracy = {:5.3f}'.format(final_acc))
+    acc = run_test(test_x, test_y)
+    acc_str = ','.join(['%5.3f']*4) % tuple(acc)
+    print('\nFinal Test : final result = {}'.format(acc_str))
+
 
 # dataset 처리, step_size 생성
 def arrange_data(batch_size):
@@ -57,6 +60,7 @@ def get_train_data(batch_size, nth):
         np.random.shuffle(shuffle_map[:test_begin_idx])     # train data 생성
     train_data = data[shuffle_map[batch_size*nth:batch_size*(nth+1)]]  # nth -> epoch idx, nth : 첫 번째 epoch, nth는 step_size를 통해 n으로 전달 위 코드 참고
     return train_data[:, :-output_cnt], train_data[:, -output_cnt:]    # train_x, train_y
+
 
 # per step process
 def run_train(x, y):
@@ -88,32 +92,46 @@ def backprop_neuralnet(G_output, x):
     g_output_w = x.transpose()
     G_w = np.matmul(g_output_w, G_output)
     G_b = np.sum(G_output, axis=0)
-
-    weight -= LERNING_RATE * G_w
-    bias -= LERNING_RATE * G_b
+    weight -= LEARNING_RATE * G_w
+    bias -= LEARNING_RATE * G_b
 
 # main 함수 정리
-def pulsar_exec(num_epochs=10, batch_size=10, report=1):
-    load_pulsar_dataset()
+def pulsar_exec(num_epochs=10, batch_size=10, report=1, adjust_ratio=False):
+    load_pulsar_dataset(adjust_ratio)   # adjust_ratio : True(별과 pulsar비율 맞춤), False(기존 비율 그대로 사용)
     init_model()
     train_and_test(num_epochs, batch_size, report)
 
+
 # dataread
-def load_pulsar_dataset():
+def load_pulsar_dataset(adjust_ratio):
+    pulsars, stars = [], []    # 펄서와 별을 각각 나누어 리스트에 저장
     with open("../Data/pulsar_stars.csv") as csvfile:
         csvreader = csv.reader(csvfile)
         next(csvreader, None)
         rows = []
         for row in csvreader:
-            rows.append(row)
+            if row[8] == '1': pulsars.append(row)
+            else : stars.append(row)
 
     global data, input_cnt, output_cnt
     input_cnt, output_cnt = 8, 1     # 입출력 벡터 크기
-    data =np.asarray(rows, dtype='float32')    # list구조는 numpy에서 제공하는 연산에 부적절, 비효율적 -> 배열구조로 변환
+
+    star_cnt, pulsar_cnt = len(stars), len(pulsars)
+
+    if adjust_ratio:
+        # true
+        data = np.zeros([2*star_cnt, 9])   # pulsar를 star수 만큼 늘릴거라 star_cnt의 두배만큼 설정
+        data[0:star_cnt, :] = np.asarray(stars, dtype='float32')
+        for n in range(star_cnt):
+            data[star_cnt+n] = np.asarray(pulsars[n%pulsar_cnt], dtype='float32')
+    else:
+        # false
+        data = np.zeros([star_cnt+pulsar_cnt, 9])
+        data[0:star_cnt, :] = np.asarray(stars, dtype='float32')
+        data[star_cnt:, :] = np.asarray(pulsars, dtype='float32') # list구조는 numpy에서 제공하는 연산에 부적절, 비효율적 -> 배열구조로 변환
 
 
 # 후처리 과정에 대한 순전파와 역전파 함수의 재정의
-
 # loss 계산
 def forward_postproc(output, y):
     entropy = sigmoid_cross_entropy_with_logits(y, output)
@@ -132,17 +150,37 @@ def backprop_postproc(G_loss, aux):
 
     return G_output
 
+
 # 이진 분류 정확도 함수
 def eval_accuracy(output, y):
     """
     np.greater([4,2],[2,2])
     array([ True, False])
     """
-    estimate = np.greater(output, 0)   # 0보다 크면 True (해당하는 array,list 원소 별)
-    answer = np.greater(y, 0.5)        # 0.5보다 크면 True (해당하는 array,list 원소 별)
-    correct = np.equal(estimate, answer)   # True, False (해당하는 array,list 원소 별)
+    est_yes = np.greater(output, 0)   # 0보다 크면 True (해당하는 array,list 원소 별)
+    ans_yes = np.greater(y, 0.5)        # 0.5보다 크면 True (해당하는 array,list 원소 별)
+    est_no = np.logical_not(est_yes)
+    ans_no = np.logical_not(ans_yes)
 
-    return np.mean(correct)   # True 비율 구해짐 (python은 참은 1, 거짓은 0이라 간주)
+    TP = np.sum(np.logical_and(est_yes, ans_yes))
+    FP = np.sum(np.logical_and(est_yes, ans_no))
+    FN = np.sum(np.logical_and(est_no, ans_yes))
+    TN = np.sum(np.logical_and(est_no, ans_no))
+
+    accuracy = safe_div(TP+TN, TP+FP+FN+TN)
+    precision = safe_div(TP, TP+FP)
+    recall = safe_div(TP, TP+FN)
+    f1 = 2 * safe_div(precision*recall, precision+recall)
+
+    # return np.mean(correct)   # True 비율 구해짐 (python은 참은 1, 거짓은 0이라 간주)
+    return [accuracy, precision, recall, f1]
+
+
+def safe_div(p, q):
+    p, q = float(p), float(q)  # type 오류 방지
+    if np.abs(q) < 1.0e-20: return np.sign(p)  # 0으로 나눠지는 것을 방지
+    return p / q
+
 
 # 활성화 함수 -> class로 정의하고 forward,backward 합치면 더 좋지 않을까?
 def relu(x):
@@ -166,20 +204,27 @@ def sigmoid_cross_entropy_with_logits_derv(z, x):
 
 if __name__ == "__main__":
     print("TRAIN START!")
-    np.random.seed(1234)
+    np.random.seed(42)
 
 
     def randomize(): np.random.seed(time.time())  # 난수 함수 초기화
 
     # hyperparameter 정의
-    RND_MEAN = 0  # 정규분포 난수값의 평균
-    RND_STD = 0.0030  # 표준편차
-    LERNING_RATE = 0.1
+    # RND_MEAN = 0  # 정규분포 난수값의 평균
+    # RND_STD = 0.0030  # 표준편차
+    # LERNING_RATE = 0.2
+
+    RND_MEAN = 0
+    RND_STD = 0.0030
+
+    LEARNING_RATE = 0.001
 
     num_epochs = 10
     batch_size = 10
     report = 1
-    pulsar_exec(num_epochs, batch_size, report)
+
+    # Loss 값도 많이 다르고 acc가 불안정함 -> 이유를 모르겠음 -> loss값은 learning_rate 차이였음
+    pulsar_exec(num_epochs, batch_size, report, adjust_ratio=True)
 
 
 
--- readme.txt
+++ readme.txt
@@ -9,3 +9,6 @@ https://github.com/konantechnology/academy.alzza
 - 2021.2.4   chapter01 끝, 해볼만 한 것 -> multi-layer로 만들어보기
 - 2021.2.6   chapter02 시작 pulsar 이진분류
 - 2021.2.8   chapter02 ~2.10
+- 2021.2.9   chapter02 완료
+
+- global 변수 문제 -> 다른 py파일에서 함수사용이 안 됨
